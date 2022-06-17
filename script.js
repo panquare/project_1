c3e8f1856a93e0df6f86dde6d2258c78b8e83954
Pavel <panquare@mail.ru>
2022-06-17 09:27:29 +0300

    add modifieds app

--- script.js
+++ script.js
@@ -7,10 +7,32 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
-const Pavel = prompt ('Один из просмотренных последних фильмов?', ''),
-      Andrei = prompt ('На сколько оцените его?', ''),
-      Anton = prompt ('Один из просмотренных последних фильмов?', ''),
-      Gera = prompt ('На сколько оцените его?', '');
-      personalMovieDB.movies[Pavel] = Andrei;
-      personalMovieDB.movies[Anton] = Gera;
-      console.log(personalMovieDB);
+
+for (let i = 0; i < 2; i++) {
+    const Pavel = prompt ('Один из просмотренных последних фильмов?', ''),
+         Andrei = prompt ('На сколько оцените его?', '');
+
+
+    if (Pavel != null && Andrei != null && Pavel != '' && Andrei  != '' && Pavel.length < 50) {
+        personalMovieDB.movies[Pavel] = Andrei;     
+        console.log('done!');
+    } else {
+        console.log('Error!');
+        i--;
+    }  
+    if (personalMovieDB.count < 10) {
+       console.log('Просмотрено довольно мало фильмов')
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if(personalMovieDB.count <= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Ошибка');
+    }
+
+
+    personalMovieDB.movies[Pavel] = Andrei;     
+    
+}
+
+    console.log(personalMovieDB);
